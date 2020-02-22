ab17d096363f0758c3a75457f79c2c9401342893
vito <vito@yeahka.com>
2020-02-22 09:48:31 +0800

    init

---
+++ src/main/java/com/laoli/kim/KimApplication.java
@@ -0,0 +1,13 @@
+package com.laoli.kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laoli/kim/KimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laoli.kim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
